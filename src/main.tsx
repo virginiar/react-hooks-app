d089d3e55884954d57b01f3a7ba20c3497d0a5c0
Virginia Redondo <7237305+virginiar@users.noreply.github.com>
2026-02-02 10:35:54 +0100

    Implementando el componente TrafficLightWithEffect empleando useEffect para cambiar el color y el contador

--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,15 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 
 // import { HooksApp } from "./HooksApp.tsx";
-import { TrafficLight } from "./01-useState/TrafficLight.tsx";
+// import { TrafficLight } from "./01-useState/TrafficLight.tsx";
+import { TrafficLightWithEffect } from "./02-useEffect/TrafficLightwithEffect.tsx";
 
 import "./index.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     {/* <HooksApp /> */}
-    <TrafficLight />
+    {/* <TrafficLight /> */}
+    <TrafficLightWithEffect />
   </StrictMode>,
 );
