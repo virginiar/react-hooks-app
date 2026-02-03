f24e66062dd14c3f3f543e3303f26ea842fb3578
Virginia Redondo <7237305+virginiar@users.noreply.github.com>
2026-02-03 09:34:30 +0100

    Implementando reductor taskReducer

---
+++ src/05-useReducer/reducer/taskReducer.ts
@@ -0,0 +1,82 @@
+interface Todo {
+    id: number;
+    text: string;
+    completed: boolean;
+}
+
+interface TaskState {
+    todos: Todo[];
+    length: number;
+    completed: number;
+    pending: number;
+}
+
+export type TaskAction =
+    | { type: 'ADD_TODO'; payload: string; }
+    | { type: 'TOGGLE_TODO'; payload: number; }
+    | { type: 'DELETE_TODO'; payload: number; };
+
+// Reducer: función que retorna siempre un nuevo estado
+// Argumentos: Estado actual y una acción
+export const taskReducer = (
+    state: TaskState,
+    action: TaskAction
+): TaskState => {
+    switch (action.type) {
+        case 'ADD_TODO': {
+            const newTodo = {
+                id: Date.now(),
+                text: action.payload,
+                completed: false,
+            };
+
+            return {
+                ...state,
+                todos: [...state.todos, newTodo],
+                length: state.todos.length + 1,
+                pending: state.pending + 1,
+            };
+        }
+
+        case 'DELETE_TODO': {
+            const currentTodos = state.todos.filter(
+                (todo) => todo.id !== action.payload
+            );
+
+            // const completedTodos = currentTodos.filter((todo) => todo.completed).length;
+            // const pendingTodos = currentTodos.length - completedTodos;
+
+            return {
+                ...state,
+                todos: currentTodos,
+                length: currentTodos.length,
+                completed: currentTodos.filter((todo) => todo.completed).length,
+                pending: currentTodos.filter((todo) => !todo.completed).length,
+            };
+        }
+
+
+        case 'TOGGLE_TODO': {
+            const updatedTodos = state.todos.map((todo) => {
+                if (todo.id === action.payload) {
+                    return { ...todo, completed: !todo.completed };
+                }
+                return todo;
+            });
+
+            return {
+                ...state,
+                todos: updatedTodos,
+                completed: updatedTodos.filter((todo) => todo.completed).length,
+                pending: updatedTodos.filter((todo) => !todo.completed).length,
+            };
+        }
+
+
+
+
+        default:
+            return state;
+    }
+
+};
