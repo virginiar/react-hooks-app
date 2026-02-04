a820a55c4fcaeda389df7f4c92400397e2e8a5e6
Virginia Redondo <7237305+virginiar@users.noreply.github.com>
2026-02-04 13:07:33 +0100

    Refactorizando InstagromApp para emplear los hooks useOptimistic y useTransition

--- src/07-useOptimistic/InstagromApp.tsx
+++ src/07-useOptimistic/InstagromApp.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useOptimistic, useState, useTransition } from "react";
 
 interface Comment {
   id: number;
@@ -6,26 +6,49 @@ interface Comment {
   optimistic?: boolean;
 }
 
+let lastId = 2;
+
 export const InstagromApp = () => {
+  const [isPending, startTransition] = useTransition();
+
   const [comments, setComments] = useState<Comment[]>([
     { id: 1, text: "¡Gran foto!" },
     { id: 2, text: "Me encanta 🧡" },
   ]);
 
+  const [optimisticComments, addOptimisticComments] = useOptimistic(
+    comments,
+    (currentComments, newCommentText: string) => {
+      lastId++;
+      return [
+        ...currentComments,
+        {
+          id: lastId,
+          text: newCommentText,
+          optimistic: true,
+        },
+      ];
+    },
+  );
+
   const handleAddComment = async (formData: FormData) => {
     const messageText = formData.get("post-message") as string;
-    console.log("Nuevo comentario", messageText);
 
-    await new Promise((resolve) => setTimeout(resolve, 3000));
-    console.log("Mensaje grabado");
+    addOptimisticComments(messageText);
 
-    setComments((prev) => [
-      ...prev,
-      {
-        id: new Date().getTime(),
-        text: messageText,
-      },
-    ]);
+    startTransition(async () => {
+      // Simular la petición http al servidor
+      await new Promise((resolve) => setTimeout(resolve, 3000));
+      // console.log("Mensaje grabado");
+
+      setComments((prev) => [
+        ...prev,
+        {
+          id: new Date().getTime(),
+          text: messageText,
+        },
+      ]);
+    });
   };
 
   return (
@@ -44,7 +67,7 @@ export const InstagromApp = () => {
 
       {/* Comentarios */}
       <ul className="flex flex-col items-start justify-center bg-gray-300 w-125 p-4">
-        {comments.map((comment) => (
+        {optimisticComments.map((comment) => (
           <li key={comment.id} className="flex items-center gap-2 mb-2">
             <div className="bg-blue-500 rounded-full w-10 h-10 flex items-center justify-center">
               <span className="text-white text-center">A</span>
@@ -71,7 +94,7 @@ export const InstagromApp = () => {
         />
         <button
           type="submit"
-          disabled={false}
+          disabled={isPending}
           className="bg-blue-500 text-white p-2 rounded-md w-full"
         >
           Enviar
